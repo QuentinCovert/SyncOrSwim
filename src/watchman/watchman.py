c0c6918325fc8086a9fe4bcf18c23a28ab7d7675
Mark Hernandez <mh81796@gmail.com>
2016-11-15 21:30:57 -0600

    Fix socket communication. Progress on subscribe/unsubscribe commands

--- src/watchman/watchman.py
+++ src/watchman/watchman.py
@@ -4,37 +4,50 @@ import socket
 
 class Watchman:
     def __init__(self):
-        self.sock_path = getSocketLocation()
+        # To be defined later.
+        self.default_trigger_command = ''
 
-    def getSocketLocation():
+    def getSocketLocation(self):
         # Use command line to retrieve socket location
         # and parse it from the JSON formatted output
-        sock_info = os.open('watchman get-sockname').read()
+        sock_info = os.popen('watchman get-sockname').read()
         sock_path = json.loads(sock_info)['sockname']
         return sock_path
 
     def socketCommunicate(self, msgObj):
+        sock_path = self.getSocketLocation()
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        s.connect(self.sock_path)
+        s.connect(sock_path)
 
         # Convert msgObj to JSON and send
         # Must be utf-8 encoded
-        msgJson = json.dumps(jsonObj) + '\n'
-        s.send(msgJson.encode())
-
+        jsonObj = json.dumps(msgObj) + '\n'
+        s.send(jsonObj.encode())
         data = s.recv(1024)
         s.close
         return data.decode()
 
-
     def checkVersion(self):
-        versionReq = ['version']
-        response = self.socketCommunicate(versionReq)
+        response = self.socketCommunicate(['version'])
+        version = json.loads(response)['version']
+        return version
+
+    def subscribe(self, path_to_root, trigger_name, ignored_files):
+        expression = [
+            ['anyof', ['type', 'f'], ['type', 'd']]
+        ]
+
+        #TODO: Add ignored files filter
+        #TODO: Add command to run on trigger
+        command = ['']
+
+    def unsubscribe(self, path_to_root, trigger_name):
+        unsubRequest = ['unsubscribe', path_to_root, trigger_name]
+        response = self.socketCommunicate(unsubRequest)
         print(response)
-        return response
 
-    def subscribe(root_directory):
-        return False
-
-    def unsubscribe(root_directory):
-        return False
+class Trigger:
+    __init__(self, name, expression, command):
+        self.name = name
+        self.expression = expression
+        self.command = command
