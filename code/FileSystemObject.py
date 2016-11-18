033555a0e87c1916174d0f4c501ec1d12feba802
QuentinCovert <QuentCov2@gmail.com>
2016-11-18 09:50:03 -0600

    Hash Verification is never used.

--- code/FileSystemObject.py
+++ code/FileSystemObject.py
@@ -20,30 +20,15 @@ class FileSystemObject(metaclass=ABCMeta):
 
 class File(FileSystemObject):
 
-    def __init__(self, path, mod, deleted, encrypted, lastSync, ePath, hash):
+    def __init__(self, path, mod, deleted, encrypted, lastSync, ePath):
         super().__init__(path, mod, deleted, encrypted, lastSync)
         self.encryptedFilePath = ePath
-        self.hash = hash
     
     def encrypt(self, crypto):
         return encrypt(crypto, self)
     
     def decrypt(self, crypto):
         return decrypt(crypto, self)
-        
-    def verify(self):
-        # Open,close, read file and calculate MD5 on its contents 
-        with open(self.fileName) as file:
-            # read contents of the file
-            data = file.read()    
-            # pipe contents of the file through
-            md5_returned = hashlib.md5(data).hexdigest()
-
-        # Finally compare original MD5 with freshly calculated
-        if orginal_md5 == md5_returned:
-            return true
-        else:
-            return false
 
 class Directory(FileSystemObject):
     
