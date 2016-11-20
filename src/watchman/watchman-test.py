736e309c2b701c27c070da7c5a613de09dcf83fe
Mark Hernandez <mh81796@gmail.com>
2016-11-19 20:09:27 -0600

    Defined subscribe and unsubscribe methods. Further testing is required

--- src/watchman/watchman-test.py
+++ src/watchman/watchman-test.py
@@ -1,5 +1,28 @@
 from watchman import Watchman
+import sys
 
-watch = Watchman()
-print("Socket Location: " + watch.getSocketLocation())
-print("Version: " + watch.checkVersion())
+if len(sys.argv) != 3:
+    print('Command-line arguments must follow this format:')
+    print('$ python watchman-test.py [rootPath] [subscriptionName]\n')
+else:
+    print(sys.argv[1])
+    print(sys.argv[2])
+
+    rootPath = sys.argv[1]
+    subscriptionName = sys.argv[2]
+
+    watch = Watchman()
+    # print('Subscribing...')
+    # watch.subscribe(rootPath, subscriptionName)
+
+    # print('Unsubscribing...')
+    # watch.unsubscribe(rootPath, subscriptionName)
+
+    print('Checking clock')
+    watch.clock(rootPath)
+
+    # print('Getting changed files')
+    # watch.since(rootPath)
+
+    # print("Socket Location: " + watch.getSocketLocation())
+    # print("Version: " + watch.checkVersion())
