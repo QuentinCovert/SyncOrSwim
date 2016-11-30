b3881be9080eaf74a4a9c1e99a14be6be5ead553
Cameron Johnson <camcamcj@gmail.com>
2016-11-30 10:06:24 -0600

    global settings added

--- src/settings/Settings.py
+++ src/settings/Settings.py
@@ -12,16 +12,32 @@ class Settings:
 
         self.data = data
 
-        self.minimum_split_size = data["minimum_split_size"]
-        self.block_size = data["block_size"]
         self.rootPath = data["rootPath"]
         self.encryptedFolder = data["encryptedFolder"]
         self.remotePath = data["remotePath"]
 
-    #static class to generate settings file
-    def generateSettings(self, minimum_split_size, block_size, rootPath, encryptedFolder, remotePath):
+        #init global settings
+        with open(currentPath + "global_settings.p", "rb") as fp:
+            global_data = pickle.load(fp)
 
-        data = {'minimum_split_size': minimum_split_size, 'block_size': block_size, 'rootPath': rootPath, 'encryptedFolder': encryptedFolder, 'remotePath': remotePath}
+        self.global_data = global_data
+
+        self.minimum_split_size = global_data["minimum_split_size"]
+        self.block_size = global_data["block_size"]
+        self.key = global_data["key"]
+
+
+    #static class to generate settings file
+    def generateLocalSettings(rootPath, encryptedFolder, remotePath):
+
+        data = {'rootPath': rootPath, 'encryptedFolder': encryptedFolder, 'remotePath': remotePath}
         currentPath = os.path.realpath(__file__)
         with open(currentPath + 'settings.p', 'wb') as fp:
             pickle.dump(data, fp)
+
+    def generateGlobalSettings(minimum_split_size, block_size, key):
+
+        global_data = {'minimum_split_size': minimum_split_size, 'block_size': block_size, 'key': key}
+        currentPath = os.path.realpath(__file__)
+        with open(currentPath + 'global_settings.p', 'wb') as fp:
+            pickle.dump(global_data, fp)
